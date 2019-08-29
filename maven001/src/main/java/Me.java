adc6914f2731f57187b4d172f044b6cea7092341
sean <pastisprologue@163.com>
2019-08-29 22:53:55 +0800

    提交maven001修改后

--- maven001/src/main/java/Me.java
+++ maven001/src/main/java/Me.java
@@ -2,4 +2,8 @@ public class Me {
     public static void main(String[] args) {
         System.out.println("i love you");
     }
+
+    public void test(){
+        System.out.println("me too");
+    }
 }
